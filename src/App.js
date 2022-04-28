473ab9fe3f767156758a6f3e837c81bd536aa382
ivansolodov <ivansolobear@gmail.com>
2022-04-28 13:04:33 +0300

    changed to use hooks

--- src/App.js
+++ src/App.js
@@ -1,91 +1,75 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import './App.css';
 
 const ENTER = 13;
 
-class App extends React.Component {
+const App = () => {
 
-  state = {
-    currentId: 0,
-    todos: {},
-    inputText: '',
-  };
+  const [currentId, setCurrentId] = useState(0);
+  const [todos, setTodos] = useState({});
+  const [inputText, setInputText] = useState('');
 
-  componentWillMount() {
-    document.addEventListener("keydown", this.onKeyPressed.bind(this));
+  useEffect(() => {
+    document.addEventListener("keydown", onKeyPressed);
+
+    return () => {
+      document.removeEventListener("keydown", onKeyPressed);
+    }
+  })
+
+  const onInputChange = (e) => {
+    setInputText(e.target.value)
   }
 
-  componentWillUnmount() {
-    document.removeEventListener("keydown", this.onKeyPressed.bind(this));
-  }
-
-  onInputChange = (e) => {
-    this.setState({
-      ...this.state,
-      inputText: e.target.value
-    })
-  }
-
-  onDeletePress = (id) => {
-    const newTodos = {...this.state.todos};
+  const onDeletePress = (id) => {
+    const newTodos = {...todos};
     delete newTodos[id];
-
-    this.setState({
-      ...this.state,
-      todos: newTodos
-    })
+    setTodos(newTodos)
   }
 
-  onDonePress = (id) => {
-    const newTodos = {...this.state.todos};
+  const onDonePress = (id) => {
+    const newTodos = {...todos};
     newTodos[id].done = !newTodos[id].done;
-
-    this.setState({
-      ...this.state,
-      todos: newTodos
-    })
+    setTodos(newTodos)
   }
 
-  onKeyPressed(e) {
-    if(e.keyCode === ENTER && this.state.inputText !== ''){
-      const newTodos = {...this.state.todos};
-      newTodos[this.state.currentId] = {
-        text: this.state.inputText,
+  const onKeyPressed = (e) => {
+    if(e.keyCode === ENTER && inputText !== ''){
+      const newTodos = {...todos};
+
+      newTodos[currentId] = {
+        text: inputText,
         done: false,
       };
 
-      this.setState({
-        ...this.state,
-        todos: newTodos,
-        inputText: '',
-        currentId: this.state.currentId +1
-      })
+      setTodos(newTodos);
+      setCurrentId(currentId +1);
+      setInputText('');
     }
   }
 
-  render() {
+
     return <div className={"wrapper"}>
       <div className={"wrapperItem"}>
-        <input value={this.inputText} onChange={(e) => this.onInputChange(e)} className={"input"}/>
+        <input value={inputText} onChange={(e) => onInputChange(e)} className={"input"}/>
       </div>
       <div className={"wrapperItem wrapperItemTodoList"}>
         <div className={"todoList"}>
-          {Object.keys(this.state.todos).map((id) => {
+          {Object.keys(todos).map((id) => {
             return (
                 <div key={id} className={"listItem"}>
-                  <div onClick={() => this.onDonePress(id)}>{this.state.todos[id].done ? '✖︎' : '✔︎'}️</div>
-                  <div className={this.state.todos[id].done? "done" : ""}>{this.state.todos[id].text}</div>
-                  <div onClick={() => this.onDeletePress(id)}>🗑</div>
+                  <div onClick={() => onDonePress(id)}>{todos[id].done ? '✖︎' : '✔︎'}️</div>
+                  <div className={todos[id].done? "done" : ""}>{todos[id].text}</div>
+                  <div onClick={() => onDeletePress(id)}>🗑</div>
                 </div>
             )
           })}
         </div>
       </div>
       <div className={"wrapperItem"}>
-        Todos left: {Object.keys(this.state.todos).filter(key => !this.state.todos[key].done).length}
+        Todos left: {Object.keys(todos).filter(key => !todos[key].done).length}
       </div>
     </div>
-  }
 }
 
 
